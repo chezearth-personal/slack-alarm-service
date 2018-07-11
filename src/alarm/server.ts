8fc0393b2d89ef1f5b6e040dce9dcd211c397349
Charles Rethman <rethman@me.com>
2018-07-11 02:05:59 +0200

    moved server over

--- src/alarm/server.ts
+++ src/alarm/server.ts
@@ -1,16 +1,33 @@
 "use strict";
 
+import * as config from "config";
 import * as schedule from "node-schedule";
 
-import { ScheduledItem } from "./scheduler/Scheduler";
+import { getNewAlarms } from "./db/crud";
+import { postSlack } from "./webhooks/slack";
+import { connectDb, DbClient } from '../common/db/connector';
 import { myStream } from "../common/helpers/winston";
+import { AlarmDb } from "../common/types/docs";
 
 
-schedule.scheduleJob('*/3 * * * * *', () => {
+schedule.scheduleJob(config.get("cron_check_alarms"), async () => {
 
-  myStream.write(`::ffff:127.0.0.1 - - [${(new Date()).toISOString()}] "This is the 3-second scheduled item"`);
+  //myStream.write(`::ffff:127.0.0.1 - - [${(new Date()).toISOString()}] "This is the 3-second scheduled item"`);
+
+
   //call controllers/getNewAlarms
-  //for each alarm, create new helpers/scheduler with webhook functionCb
+  const alarmsToSend: AlarmDb[] = await getNewAlarms(new Date());
+
   //webhooks/postSlack function
+  if(alarmsToSend.length > 0) {
+    const res: string[] = await Promise.all(alarmsToSend.map(async (e) => await postSlack(e.name)));
+    myStream.write(`::ffff:127.0.0.1 - - [${(new Date()).toISOString()}] "sending ${res.length} alarm messag${res.length > 1 ? 'e' : 'es'} to slack from '${config.get("slack_username")}' on the ${config.get("slack_channel")} channel" "results: ${res}"`);
+  }
 
 });
+
+
+// Mongo DB connection. Returned as a promise which resolves quite quickly. The promise is awaited each time the connection is used.
+const url: string = config.get("mongoUrl") || "mongodb://127.0.0.1:27017";
+const dbName: string = config.get("database") || "alarmServer";
+export const mongoDb: Promise<DbClient> = connectDb(url, dbName);
