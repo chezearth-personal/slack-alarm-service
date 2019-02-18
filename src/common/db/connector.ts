46d48ad840873ddf33869ed056c7ad9a7838c687
chezearth <rethman@me.com>
2019-02-18 09:17:48 +0200

    Updated DB connector

--- src/common/db/connector.ts
+++ src/common/db/connector.ts
@@ -8,27 +8,18 @@ import { DbConfig, DbConnect } from '../types/types';
 
 
 export async function connectDb(dbConnect: DbConnect): Promise<Db> {
-
   const setTimeoutPromise = util.promisify(setTimeout);
 
   try {
-
     await setTimeoutPromise(dbConnect.wait);
-
     const client = await MongoClient.connect(dbConnect.url, { useNewUrlParser: true });
     // logger.write(`"connected to database ${uri}/${dbName}`)
-
     return client.db(dbConnect.dbName);
-
   } catch(e) {
-
-
 //       logger.write(`                 ${errorPrefix} "${e.message}" "${e.status}"
 // ${e.stack}`,"error")
     return Promise.reject(e);
-
   }
-
 
 }
 
@@ -38,21 +29,18 @@ export async function dbConnection(dbConfig: DbConfig): Promise<Db> {
   if(dbConfig.env !== 'test') logger.write(`"Attempt ${dbConfig.count + 1} connecting to database: ${dbConfig.url}" "${dbConfig.env} environment"`);
 
   try {
-
     const dbConnect = {
       url: dbConfig.url,
       dbName: dbConfig.dbName,
       wait: dbConfig.wait ? dbConfig.wait : 0
-    }
+    };
     const db: Db = await connectDb(dbConnect);
 
     if(dbConfig.env !== "test") logger.write(`"Server connected to database: ${dbConfig.url}" "${dbConfig.env} environment"`);
 
     // Now there is a database connection, we can call the server functions that come after the connection is established
     await Promise.all(dbConfig.serverFunctions.map(e => e()));
-
     return db;
-
   } catch(e) {
 
     if(dbConfig.retries && dbConfig.count + 1 < dbConfig.retries) {
